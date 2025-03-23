6134bd0d7aabcfee4903c116b5eb8b817bb3e9e1
Gabriel Simon <gabriel.ribeiro@mathex.com.br>
2025-03-23 16:48:46 -0300

    feat: enhance input handling and LP format file creation with load prices

--- utils.c
+++ utils.c
@@ -2,31 +2,51 @@
 #include <stdio.h>
 
 
-void readInputFile(int *comps, int *loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[]) {
-    scanf("%d %d", comps, loads);
-    for (int i = 0; i < *comps; i++) {
+void readInputFile(int comps, int loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[], int loadPrices[]) {
+    for (int i = 0; i < comps; i++) {
         scanf("%d %d", &compWeights[i], &compVolumes[i]);
     }
-    for (int i = 0; i < *loads; i++) {
-        scanf("%d %d", &loadWeights[i], &loadVolumes[i]);
+    for (int i = 0; i < loads; i++) {
+        scanf("%d %d %d", &loadWeights[i], &loadVolumes[i], &loadPrices[i]);
     }
+
+    printf("carregou");
 }
 
-void createLpSolveFormatFile(int comps, int loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[]) {
+void createLpSolveFormatFile(int comps, int loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[], int loadPrices[]) {
     FILE *file = fopen("lp_problem.lp", "w");
     fprintf(file, "max: ");
-    for (int i = 0; i < comps; i++) {
-        fprintf(file, "%d x%d", compWeights[i], i);
-        if (i < comps - 1) {
-            fprintf(file, " + ");
+    for (int i = 0; i < loads; i++) {
+        fprintf(file, "%d x%d ", loadPrices[i], i);
+        if (i != loads - 1) {
+            fprintf(file, "+ ");
         }
     }
     fprintf(file, ";\n");
-    for (int i = 0; i < loads; i++) {
-        fprintf(file, "%d x%d <= %d;\n", loadWeights[i], i, loadVolumes[i]);
-    }
+
     for (int i = 0; i < comps; i++) {
+        for (int j = 0; j < loads; j++) {
+            fprintf(file, "%d x%d ", loadVolumes[j], j);
+            if (j != loads - 1) {
+                fprintf(file, "+ ");
+            }
+        }
+        fprintf(file, "<= %d;\n", compVolumes[i]);
+    }
+
+    for (int i = 0; i < comps; i++) {
+        for (int j = 0; j < loads; j++) {
+            fprintf(file, "%d x%d ", loadWeights[j], j);
+            if (j != loads - 1) {
+                fprintf(file, "+ ");
+            }
+        }
+        fprintf(file, "<= %d;\n", compWeights[i]);
+    }
+
+    for (int i = 0; i < loads; i++) {
         fprintf(file, "x%d >= 0;\n", i);
     }
+
     fclose(file);
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,11 @@
+#ifdef _UTILS_H_
+#define _UTILS_H_
+
 #include <stdio.h>
 
 
-void readInputFile(int *comps, int *loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[]);
+void readInputFile(int comps, int loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[], int loadPrices[]);
 
-void createLpSolveFormatFile(int comps, int loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[]);
+void createLpSolveFormatFile(int comps, int loads, int compWeights[], int compVolumes[], int loadWeights[], int loadVolumes[], int loadPrices[]);
+
+#endif
