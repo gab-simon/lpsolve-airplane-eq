be211c927bdb46b7cb1278fee22c6b361012aed4
Gabs <gabrielafanaia@gmail.com>
2025-04-05 19:25:46 -0300

    ta pronto

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Utilize os comandos:
 make
 
 ./carga < tests/load1.txt > output.lp
+load2.txt
+load3.txt
 
 lp_solve output.lp
--- utils.c
+++ utils.c
@@ -23,65 +23,74 @@ void objectiveFunction(int k, int n, int *g) {
 	}
 	printf("\n");
 }
+#include <stdio.h>
 
+void restriction_weight(int k, int n, int *w) {
+    printf("\n");
+    for (int i = 1; i <= k; i++) {
+        for (int j = 1; j <= n; j++) {
+            printf("x%d%d", i , j );
+            if (j != n ) printf(" + ");
+        }
+        printf(" <= %d;\n", w[i]);
+    }
+}
+
+void restriction_volume(int k, int n, int *v, int *p, int *t) {
+    printf("\n");
+    for (int i = 1; i <= k; i++) {
+        for (int j = 1; j <= n; j++) {
+            printf("%f x%d%d ", (double)t[j]/p[j], i, j);
+            if (j < n) printf("+ ");
+        }
+        printf("<= %d;\n", v[i]);
+    }
+}
+
+void restriction_availability(int k, int n, int *p) {
+    printf("\n");
+    for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= k; i++) {
+            printf("x%d%d", i , j );
+            if (i != k) printf( " + ");
+        }
+        printf(" <= %d;\n", p[j]);
+    }
+}
+
+void restriction_balance(int k, int n, int *w) {
+    printf("\n");
+    int soma = 0;
+    for (int i = 1; i <= k; i++) {
+        soma += w[i];
+    }
+
+    for (int i = 1; i <= k; i++) {
+        for (int j = 1; j <= n; j++) {
+            printf("%.8f x%d%d - x%d%d ", (double)w[i]/soma , (i % k) + 1, j, i, j);
+            if ( j != n ) printf(" + ");
+        }
+        printf(" <= 0;\n");
+    }
+}
+
+void restriction_positive(int k, int n) {
+    printf("\n");
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("x%d%d >= 0; ", i + 1, j + 1);
+        }
+        printf("\n");
+    }
+}
 
 void restrictions(int k, int n, int *w, int *v, int *p, int *t, int *g) {
-
-	printf("\n");
-	// Restrição de peso
-	for (int i = 1; i <= k; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("x%d%d", i , j );
-			if (j != n ) printf(" + ");
-		}
-		printf(" <= %d;\n", w[i]);
-	}
-
-	printf("\n");
-	// restrição de volume
-	for (int i = 1; i <= k; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%f x%d%d ", (double)t[j]/p[j], i, j);
-			if (j < n) printf("+ ");
-		}
-		printf("<= %d;\n", v[i]);
-	}
-
-	printf("\n");
-	// Restrição de disponibilidade das cargas
-	for (int j = 1; j <= n; j++) {
-		for (int i = 1; i <= k; i++) {
-			printf("x%d%d", i , j );
-			if (i != k) printf( " + ");
-		}
-		printf(" <= %d;\n", p[j]);
-	}
-
-	// restrição equilíbrio de cargas
-	printf("\n");
-	int soma = 0;
-	for (int i = 1; i <= k; i++) {
-		soma += w[i];
-	}
-
-	for (int i = 1; i <= k; i++) {
-		for (int j = 1; j <= n; j++) {
-			printf("%.8f x%d%d - x%d%d ", (double)w[i] / soma, (i % k) + 1, j, i, j);
-			if ( j != n ) printf(" + ");
-		}
-		
-		printf(" <= 0;\n");
-	}
-
-	printf("\n");
-	// Restrição para as cargas alocadas serem positivas
-	for (int i = 0; i < k; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("x%d%d >= 0; ", i + 1, j + 1);
-		}
-		printf("\n");
-	}
-
-
+    restriction_weight(k, n, w);
+    restriction_volume(k, n, v, p, t);
+    restriction_availability(k, n, p);
+    restriction_balance(k, n, w);
+    restriction_positive(k, n);
 }
+
+
 
